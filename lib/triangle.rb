8d9e356dae0310c7e4ffa4624de620d1f556167f
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 20:34:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     r=[@a,@b,@c]
   end
   def kind
-  if @a+@b<@c ||@a+@c<@b||@b+@c<@a 
+  if @a+@b<@c ||@a+@c<@b||@b+@c<@a
     raise TriangleError
   elsif @a==@b && @b==@c
       :equilateral
@@ -17,8 +17,8 @@ class Triangle
       :isosceles
     elsif  @a!=@b && @b!=@c
       :scalene
-    
-      
+
+
     end
 end
 end
