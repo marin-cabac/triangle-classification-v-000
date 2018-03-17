dcc2af6038c1b6f657c4b1f1405100d40e2b429f
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 21:17:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,10 @@ class Triangle
     r=[@a,@b,@c]
   end
   def kind
-  if (@a+@b<@c ||@a+@c<@b||@b+@c<@a)
+    if a < 0 || b < 0 || c < 0
+    raise TriangleError, "a triangle should not have a side with a negative value." 
+
+  elsif (@a+@b<@c ||@a+@c<@b||@b+@c<@a)
     raise TriangleError
   elsif @a==@b && @b==@c
       :equilateral
