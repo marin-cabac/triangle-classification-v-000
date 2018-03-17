5b22b7f61439c7507a3c9fafd7fbe3badf76f849
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 16:59:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,12 +7,12 @@ class Triangle
     @c=c
   end
   def kind
-    
+
     if @a==@b && @b==@c
       :equilateral
     elsif @b==@c ||@a==@c||@a==@b
       :isosceles
-    elsif  @a!=@b && @b!=@c && @a!=@c 
+    elsif  @a!=@b && @b!=@c && @a!=@c
       :scalene
     end
 end
