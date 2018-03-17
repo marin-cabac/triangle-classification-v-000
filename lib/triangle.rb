4fcf9ab221e527438d78e30516b101ffb95571e3
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 16:47:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,13 @@
 class Triangle
+  attr_accessor :a,:b,:c
   # write code here
-  def kind(a,b,c)
-    if a==b && b==c
-      :equilateral
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
   end
+  def kind
+    if @a==@b && @b==@c
+      :equilateral
 end
 end
