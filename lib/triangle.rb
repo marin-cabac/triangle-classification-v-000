8d85ae96d9f1e591849d50ca346077e97c8cfe60
Marin Cabac <marin.cabac@gmail.com>
2018-03-17 22:52:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,13 @@ class Triangle
   end
   def kind
     min, min2, max = @arr.sort
-   if @a < 0 || @b < 0 || @c < 0 || min + min2 <= max
+   if a < 0 || b < 0 || c < 0 || min + min2 <= max
      raise TriangleError
     end
 
     if a == b && b == c
         :equilateral
-     elsif @a == @b || @a == @c || @b == @c
+     elsif a == b || a == c || b == c
        :isosceles
      else
        :scalene
